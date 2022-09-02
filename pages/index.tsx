c08e638937d1921de17ffcc8f16d592ca1d72dff
Shenghang Zhu <67240191+zyhzsh@users.noreply.github.com>
2022-09-02 11:16:26 +0200

    feat: setup tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from 'next';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <h1>hhhh</h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
